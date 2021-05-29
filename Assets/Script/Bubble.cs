75a52da68f8b19a968ebb1fa7b035120a3156d44
kksumm123 <kksumm123@gmail.com>
2021-05-30 00:27:34 +0900

    으아 왜 발사가 안되는고야

--- Assets/Script/Bubble.cs
+++ Assets/Script/Bubble.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public class Bubble : MonoBehaviour
 {
+    Rigidbody2D rigid;
+    Animator anim;
+    public int moveForwardFrame = 60;
+    public int currentFrame = 0;
+    public float speed = 0.7f;
+    public float gravityScale = -0.2f;
     void Start()
     {
-        
+        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        rigid.gravityScale = 0;
     }
 
     void Update()
     {
-        
+        var pos = transform.position;
+        while (currentFrame++ < moveForwardFrame)
+        {
+            pos.x += speed * transform.forward.z;
+        }
+
+        rigid.gravityScale = gravityScale;
+        SetAnimation("Normal");
     }
+    #region Animator
+    void SetAnimation(string value)
+    {
+        anim.Play(value);
+    }
+    #endregion
 }
