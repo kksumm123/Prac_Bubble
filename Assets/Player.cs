315864efd293ce8daf3b12804c3fde93f49cebee
kksumm123 <kksumm123@gmail.com>
2021-05-29 20:25:59 +0900

    Player 스크립트 State, isTrigger 추가

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
 {
     Rigidbody2D rigid;
     Animator anim;
+    CircleCollider2D col;
     enum StateType
     {
         Ground,
@@ -15,24 +16,75 @@ public class Player : MonoBehaviour
         DownFall
     }
     StateType state;
-    [SerializeField] StateType State
+    [SerializeField]
+    StateType State
     {
         get { return state; }
         set { state = value; }
     }
-    
+
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
-
+        col = GetComponent<CircleCollider2D>();
     }
 
     void Update()
     {
+        CurState();
         Move();
         Jump();
     }
+    #region State
+    void SetState(StateType _state)
+    {
+        state = _state;
+    }
+
+    private void CurState()
+    {
+        if (state == StateType.Jump && rigid.velocity.y < 0)
+            SetState(StateType.JumpFall);
+
+        if (state == StateType.JumpFall && isGround())
+            SetState(StateType.Ground);
+    }
+
+    public LayerMask wallLayer;
+    public float chkGroundOffsetX = 0.4f;
+    private bool isGround()
+    {
+        if (ChkGroundRay(transform.position) == false)
+            return false;
+        if (ChkGroundRay(transform.position + new Vector3(-chkGroundOffsetX, 0, 0)) == false)
+            return false;
+        if (ChkGroundRay(transform.position + new Vector3(chkGroundOffsetX, 0, 0)) == false)
+            return false;
+
+        return true;
+
+        bool ChkGroundRay(Vector3 pos)
+        {
+            // true = Ground
+            // false = Aerial
+            var hit = Physics2D.Raycast(pos, new Vector2(0, -1), 1.1f, wallLayer);
+            Debug.Assert(hit.transform != null, "wallLayer ÁöÁ¤¾ÈµÊ");
+            return (hit.transform != null);
+        }
+    }
+    #endregion
+    #region istrigger
+    void SetTrigger(bool value)
+    {
+        col.isTrigger = value;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        SetTrigger(false);
+    }
+    #endregion
+
     public float speed = 0.1f;
     private void Move()
     {
@@ -62,16 +114,20 @@ public class Player : MonoBehaviour
     public float jumpForce = 1100f;
     private void Jump()
     {
-        if (state != StateType.Ground)
-            return;
-
         // ÈûÀ» Áà¾ß ÇÔ
         // º®À» ¶Õ¾î¾ß ÇÔ
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            rigid.AddForce(new Vector2(0, jumpForce));
-            state = StateType.Jump;
-        }
+        // ¶³¾îÁú¶© ¶ÕÀ¸¸é ¾ÈµÊ
 
+
+        if (state == StateType.Ground)
+        {
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                SetTrigger(true);
+                rigid.AddForce(new Vector2(0, jumpForce));
+                SetState(StateType.Jump);
+            }
+        }
     }
+
 }
